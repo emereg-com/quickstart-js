ad0a8005a105c1da413628b0f748ad30b0a42104
edwin <edwin@ocyedwin.com>
2024-08-09 13:09:13 +0800

    playwright-core working

--- app/api/finance/dbs/v1/sessions/route.ts
+++ app/api/finance/dbs/v1/sessions/route.ts
@@ -1,5 +1,7 @@
 "use server";
 
+import { chromium } from 'playwright-core';
+
 const options = {
     method: 'POST',
     headers: {
@@ -16,6 +18,16 @@ export async function POST(req: Request) {
 
     const response = await fetch('https://emereg.com/api/v1/sessions', options)
     const data = await response.json();
+
+    console.log(data['browser_id'])
+    const endpointURL = `wss://browser.emereg.com/devtools/browser/${data['browser_id']}`
+    const browser = await chromium.connectOverCDP(endpointURL);
+
+    const contexts = browser.contexts()
+    const pages = contexts[0].pages()
+    let page = pages[0]
+
+    await page.goto('https://emereg.com')
 
     return new Response(JSON.stringify(data), {
         headers: { 'content-type': 'application/json' }
