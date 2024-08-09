9930ba37ff2d3ca90dec0b22a5ffc9a238864ba4
edwin <edwin@ocyedwin.com>
2024-08-09 12:48:30 +0800

    create Emereg session

--- app/api/finance/dbs/v1/sessions/route.ts
+++ app/api/finance/dbs/v1/sessions/route.ts
@@ -1,7 +1,24 @@
 "use server";
 
+const options = {
+    method: 'POST',
+    headers: {
+        Authorization: `Bearer ${process.env.EMEREG_API_TOKEN}`,
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+        project_id: 68,
+        timeout: 60
+    })
+};
+
 export async function POST(req: Request) {
-    return new Response(JSON.stringify({'hello': 'world'}), {
-        headers: { 'content-type': 'application/json' },
+
+    const response = await fetch('https://emereg.com/api/v1/sessions', options)
+    const data = await response.json();
+
+    return new Response(JSON.stringify(data), {
+        headers: { 'content-type': 'application/json' }
     });
+
 }
